271737a533c39b6828e98613b7a4b925bd2d7d53
mahesh95prabhu <101814052+mahesh95prabhu@users.noreply.github.com>
2023-02-24 10:30:05 +0530

    Update hello.tf

--- hello.tf
+++ hello.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-  AWS_ACCESS_KEY_ID   = "${{ secrets.AWS_ACCESS_KEY_ID }}"
-  AWS_SECRET_ACCESS_KEY  = "${{ secrets.AWS_SECRET_ACCESS_KEY }}"
   }
 
 data "aws_eks_clusters" "example" {}
