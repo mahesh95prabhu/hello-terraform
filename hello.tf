2d74ac4c3ae9047067a64197fe4681824a45828a
mahesh95prabhu <101814052+mahesh95prabhu@users.noreply.github.com>
2023-02-24 09:45:51 +0530

    Update hello.tf

--- hello.tf
+++ hello.tf
@@ -3,10 +3,16 @@ provider "aws" {
   }
 
 data "aws_eks_clusters" "example" {}
+data "aws_iam_users" "users" {}
+
 
 output "hello_world" {	
 	value = "Hello, World!"
 }
 output "eks" {	
 	value = data.aws_eks_clusters.example.name
+}
+
+output "users" {	
+	value = data.aws_iam_users.users.name
 }
