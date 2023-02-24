8451a013b4a35250fc82fa21624f7b312a83ca42
mahesh95prabhu <101814052+mahesh95prabhu@users.noreply.github.com>
2023-02-24 09:42:57 +0530

    Update hello.tf

--- hello.tf
+++ hello.tf
@@ -1,3 +1,12 @@
+provider "aws" {
+  region     = "ap-south-1"
+  }
+
+data "aws_eks_clusters" "example" {}
+
 output "hello_world" {	
 	value = "Hello, World!"
+}
+output "eks" {	
+	value = data.aws_eks_clusters.example.name
 }
