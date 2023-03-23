9d7b7f8d2ea71e29a8a79072e96980a66190ec3e
mahesh95prabhu <101814052+mahesh95prabhu@users.noreply.github.com>
2023-03-23 11:36:02 +0530

    Update hello.tf

--- hello.tf
+++ hello.tf
@@ -1,18 +1,40 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
 provider "aws" {
-  region     = "ap-south-1"
+  region = "ap-south-1"
+}
+
+
+
+resource "aws_spot_instance_request" "cheap_worker" {
+  ami           = "ami-0d81306eddc614a45"
+  spot_price    = "0.01"
+  instance_type = "t3a.nano"
+  availability_zone = "ap-south-1a"
+  spot_type = "one-time"
+  block_duration_minutes= 60
+  tags = {
+    Name = "CheapWorker"
+  }
+}
+
+data "aws_instances" "test" {
+  instance_tags = {
+    Role = "CheapWorker"
   }
 
-data "aws_eks_clusters" "example" {}
-data "aws_iam_users" "users" {}
-
-
-output "hello_world" {	
-	value = "Hello, World!"
-}
-output "eks" {	
-	value = data.aws_eks_clusters.example.names
-}
-
-output "users" {	
-	value = data.aws_iam_users.users.names
+  instance_state_names = ["running"]
+}output ec2 {
+  value       = data.aws_instances.id
+  sensitive   = true
+  description = "description"
+  depends_on  = []
 }
